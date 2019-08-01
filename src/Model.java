5bae5879c7f164f02a60761403b6284d2865be2e
tom <ta.luxton@gmail.com>
2019-08-01 11:23:32 +1000

    test commit

--- src/Model.java
+++ src/Model.java
@@ -178,8 +178,7 @@ public class Model {
             playerMoney += getStockValue() * sellAmount;
             playerStocksOwned -= sellAmount;
         } else {
-            ;// need to throw some kind of error here if its too much
+            ;// need to throw some kind of error here if its to0 much
         }
     }
-
 }
